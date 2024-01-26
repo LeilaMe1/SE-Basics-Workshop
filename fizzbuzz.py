01f8301f59a553a09087ff1c916ae77fcbe2f190
LeilaMe1 <arcticmonkey.lm@gmail.com>
2024-01-26 18:45:51 -0500

    working fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,9 @@
-for i in range(20):
-    print(i+1)
+for foo in range(1,21):
+    if (foo % 3 == 0):
+        print("Fizz")
+    if (foo % 5 == 0):
+        print("Buzz")
+    if ((foo % 3 == 0) and (foo % 5 == 0)):
+        print("FizzBuzz")
+    if (foo % 3 != 0) and (foo % 5 != 0):
+        print(foo)
